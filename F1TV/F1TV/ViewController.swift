bf2e122578cca910931333a591f9cf1330737b79
Noah Fetz <noah@fetz-gr.ch>
2020-10-24 15:03:20 +0200

    [Init] Initial commit

---
+++ F1TV/F1TV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  F1TV
+//
+//  Created by Noah Fetz on 24.10.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
